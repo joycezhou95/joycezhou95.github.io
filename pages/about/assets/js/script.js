8ab422517ae7fafd81c0ed605f4643266406fe10
joycezhou95 <joyce.zhou@berkeley.edu>
2015-11-25 14:07:10 -0800

    remove border left

--- pages/about/assets/js/script.js
+++ pages/about/assets/js/script.js
@@ -1,21 +1,14 @@
 $(document).ready(function(){
 
 	$("#overview").click(function() {
-		$("#overview").css("border-left","solid");
-		$("#academics").css("border-left", "none");
-		$("#art-design").css("border-left", "none");
-		$("#personal").css("border-left", "none");
 	    $('html,body').animate({
 	        scrollTop: $("#overview-paragraph").offset().top - 100},
 	        'slow');
 	});
 
 
-	$("#academics").click(function() {
-		$("#overview").css("border-left","none");
-		$("#academics").css("border-left", "solid");
-		$("#art-design").css("border-left", "none");
-		$("#personal").css("border-left", "none");	    $('html,body').animate({
+	$("#academics").click(function() {  
+		$('html,body').animate({
 	        scrollTop: $("#academic-paragraph").offset().top - 100},
 	        'slow');
 
@@ -23,10 +16,6 @@ $(document).ready(function(){
 
 
 	$("#art-design").click(function() {
-		$("#overview").css("border-left","none");
-		$("#academics").css("border-left", "none");
-		$("#art-design").css("border-left", "solid");
-		$("#personal").css("border-left", "none");
 	    $('html,body').animate({
 	        scrollTop: $("#art-paragraph").offset().top - 100},
 	        'slow');
@@ -34,14 +23,9 @@ $(document).ready(function(){
 
 
 	$("#personal").click(function() {
-		$("#overview").css("border-left","none");
-		$("#academics").css("border-left", "none");
-		$("#art-design").css("border-left", "none");
-		$("#personal").css("border-left", "solid");
 	    $('html,body').animate({
 	        scrollTop: $("#personal-paragraph").offset().top - 100},
 	        'slow');
 	});
-	$("#academics").css("border-left","5px");
 
 });
